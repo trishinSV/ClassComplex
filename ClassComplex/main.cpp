61d3eb7a170a7603df9c31e1121cebac1539ee78
trishinSV <Sergey@MacBook-Pro-Sergej.local>
2017-04-20 20:39:52 +0300

    Initial Commit

---
+++ ClassComplex/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ClassComplex
+//
+//  Created by Сергей Тришин on 20.04.17.
+//  Copyright © 2017 Сергей Тришин. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
